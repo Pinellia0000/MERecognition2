d410dbbdac81f3fd2b7780bce7000c643d486f91
pinellia0000 <pinellia0000@163.com>
2026-03-19 08:24:48 +0800

    Update train_classify_SKD_TSTSAN_functions_4.py

--- train/train_classify_SKD_TSTSAN_functions_4.py
+++ train/train_classify_SKD_TSTSAN_functions_4.py
@@ -14,9 +14,9 @@ from collections import OrderedDict
 import shutil
 import sys
 # 注意修改
-from model.all_model_6 import *
+from model.all_model_7 import *
 
-all_model_path = "/kaggle/working/MERecognition2/model/all_model_6.py"
+all_model_path = "/kaggle/working/MERecognition2/model/all_model_7.py"
 
 
 def reset_weights(m):  # Reset the weights for network to avoid weight leakage
